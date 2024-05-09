07923011c9053534a5cde24294fb3d127f91af95
Alexander Beniaminov <abeniaminov@gmail.com>
2024-05-09 21:05:36 +0300

    Update client.go

--- smpp/client.go
+++ smpp/client.go
@@ -12,8 +12,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/mdouchement/smpp/smpp/pdu"
-	"github.com/mdouchement/smpp/smpp/pdu/pdufield"
+	"github.com/abeniaminov/smpp/smpp/pdu"
+	"github.com/abeniaminov/smpp/smpp/pdu/pdufield"
 )
 
 // ConnStatus is an abstract interface for a connection status change.
